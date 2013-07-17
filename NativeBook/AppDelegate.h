445dbb11b64c5f6a68b6f1c2b0a9ef1620d91d3f
Jean-André Santoni <jean.andre.santoni@gmail.com>
2013-07-17 20:01:47 +0200

    Initial Commit

---
+++ NativeBook/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NativeBook
+//
+//  Created by Jean-André Santoni on 17/07/13.
+//  Copyright (c) 2013 Jean-André Santoni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ NativeBook/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NativeBook
+//
+//  Created by Jean-André Santoni on 17/07/13.
+//  Copyright (c) 2013 Jean-André Santoni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
